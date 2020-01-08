de561f6bbc3a663f38debb768fd43ff9c65a2ee4
e1kaaa <e1kaczrw@gmail.com>
2020-01-08 17:39:18 +0100

    enemy ai - guarding

--- RPG/Assets/Scripts/Control/AIController.cs
+++ RPG/Assets/Scripts/Control/AIController.cs
@@ -1,5 +1,7 @@
 ﻿using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,31 +11,106 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 3f;
+        [SerializeField] PatrolPath patrolPath;
+        [SerializeField] float waypointTolerance=1f;
+        [SerializeField] float waypointDwellingTime = 1.5f;
         GameObject player;
         Fighter fighter;
+        Mover mover;
         Health health;
+
+        Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
+        float timeSinceReachWaypoint = Mathf.Infinity;
+        private int currentWaypointIndex;
+
         void Start()
         {
             fighter = GetComponent<Fighter>();
             player = GameObject.FindWithTag("Player");
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+            guardPosition = transform.position;
         }
 
         void Update()
         {
             if (health.IsDead()) return;
-            if (DistanceToPlayer()<chaseDistance && fighter.CanAttack(player))
+            if (DistanceToPlayer() < chaseDistance && fighter.CanAttack(player))
             {
-                 fighter.Attack(player);
+                AttackBehaviour();
+            }
+            else if (timeSinceLastSawPlayer < suspicionTime)
+            {
+                SuspicionBehaviour();
             }
             else
             {
-                fighter.Cancel();
+                PatrolBehaviour();
+            }
+            UpdateTimes();
+        }
+
+        private void AttackBehaviour()
+        {
+            timeSinceLastSawPlayer = 0;
+            fighter.Attack(player);
+        }
+
+        private void UpdateTimes()
+        {
+            timeSinceLastSawPlayer += Time.deltaTime;
+            timeSinceReachWaypoint += Time.deltaTime;
+        }
+
+        private void PatrolBehaviour()
+        {
+            Vector3 nextPosition = guardPosition;
+            if (patrolPath != null)
+            {
+                if (AtWayPoint())
+                {
+                    timeSinceReachWaypoint = 0;
+                    CycleWayPoint();
+                }
+                nextPosition = GetCurrentWayPoint();
+            }
+            if (timeSinceReachWaypoint > waypointDwellingTime)
+            { 
+                mover.StartMoveAction(nextPosition); 
             }
         }
+
+        private Vector3 GetCurrentWayPoint()
+        {
+            return patrolPath.GetWayPoint(currentWaypointIndex);
+        }
+
+        private void CycleWayPoint()
+        {
+            currentWaypointIndex = patrolPath.GetNextIndex(currentWaypointIndex);
+        }
+
+        private bool AtWayPoint()
+        {
+            float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWayPoint());
+            return distanceToWaypoint < waypointTolerance;
+        }
+
+        private void SuspicionBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
         private float DistanceToPlayer()
         {
             return Vector3.Distance(transform.position, player.transform.position);
+        }
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position,chaseDistance);
         }
     }
 }
---
+++ RPG/Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        const float gizmosRadius = .2f;
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWayPoint(i), gizmosRadius);
+                Gizmos.DrawLine(GetWayPoint(i), GetWayPoint(j));
+            }
+        }
+
+        public  int GetNextIndex(int i)
+        {
+            //if(i < transform.childCount - 1)
+            //{
+            //    return i + 1;
+            //}
+            //else
+            //{
+            //    return 0;
+            //}
+            if (i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+            else
+            {
+                return i + 1; 
+            }
+        }
+
+        public Vector3 GetWayPoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+    }
+}
